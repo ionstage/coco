318266c18f577002f70c7d066f63cdf4b4926c0b
nasitra <nasitra@users.noreply.github.com>
2018-12-29 08:12:09 +0900

    Remove 'main' component

--- js/app.js
+++ js/app.js
@@ -1,9 +1,1 @@
 import './components/app-content.js'
-import Main from './components/main.js'
-
-const main = new Main({ element: document.body })
-
-// for debug
-if (typeof global === 'undefined') {
-  window.app = { main }
-}
--- js/components/main.js
+++
@@ -1,7 +0,0 @@
-const jCore = require('jcore')
-
-export default class extends jCore.Component {
-  constructor (props) {
-    super(props)
-  }
-}
