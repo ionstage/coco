7f14ebbe8c541a106bb3731878b20e434174940e
nasitra <nasitra@users.noreply.github.com>
2019-01-10 20:31:12 +0900

    Add input element to coco-textinput component

--- components/coco-textinput.js
+++ components/coco-textinput.js
@@ -1,3 +1,13 @@
-class CocoTextInput extends HTMLElement {}
+const template = document.createElement('template')
+
+template.innerHTML = `<input type="text" autocapitalize="off" autocomplete="off" autocorrect="off" spellcheck="false">`
+
+class CocoTextInput extends HTMLElement {
+  constructor () {
+    super()
+    this.attachShadow({ mode: 'open' })
+    this.shadowRoot.appendChild(template.content.cloneNode(true))
+  }
+}
 
 window.customElements.define('coco-textinput', CocoTextInput)
