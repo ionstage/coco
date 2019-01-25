ec0fe903aa132e6d44257a3d3cf37162c4fccdba
nasitra <nasitra@users.noreply.github.com>
2019-01-25 19:27:14 +0900

    Add button element to coco-button component

--- components/coco-button.js
+++ components/coco-button.js
@@ -1,3 +1,13 @@
-class CocoButton extends HTMLElement {}
+const template = document.createElement('template')
+
+template.innerHTML = `<button></button>`
+
+class CocoButton extends HTMLElement {
+  constructor () {
+    super()
+    this.attachShadow({ mode: 'open' })
+    this.shadowRoot.appendChild(template.content.cloneNode(true))
+  }
+}
 
 window.customElements.define('coco-button', CocoButton)
