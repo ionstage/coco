b739b5602489be0de7aa378752bdf5dfa0833fe3
nasitra <nasitra@users.noreply.github.com>
2019-02-24 17:08:14 +0900

    Add textarea element to coco-textarea component

--- components/coco-textarea.js
+++ components/coco-textarea.js
@@ -1,3 +1,13 @@
-class CocoTextarea extends HTMLElement {}
+const template = document.createElement('template')
+
+template.innerHTML = `<textarea></textarea>`
+
+class CocoTextarea extends HTMLElement {
+  constructor () {
+    super()
+    this.attachShadow({ mode: 'open' })
+    this.shadowRoot.appendChild(template.content.cloneNode(true))
+  }
+}
 
 window.customElements.define('coco-textarea', CocoTextarea)
